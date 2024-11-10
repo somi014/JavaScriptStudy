69ca1631ea74dd20723ea21757a5e2bb8f13e95e
somi014 <somi112005@gmail.com>
2024-11-10 16:36:09 +0900

    null 병합 연산자

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-var elem = null;
+var foo = null ?? 'default string';
+console.log(foo);   //default string
 
-var value = elem?.value;
-console.log(value);     //undefined
+//논리 연산자를 사용한 단축 평가의 경우 좌항의 연산자가 Falsy값이면
+//우항의 피연산자를 반환한다.
+//만약 Falsy 값인 0이나 ''도 기본값으로서 유요하다면 예기치 않은 동작이 발행할 수 있다.
+foo = '' || 'default string';
+console.log(foo);   //default string
 
-value = elem && elem.value;
-console.log(value);     //null
-
-//논리 연산자는 좌항 피연산자가 false로 평가되는 Falsy값이면
-//좌항 피연산자를 그대로 반환한다.
-//하지만 0 이나 ''은 객체로 평가될 떄도 있다.
-var str = '';
-
-var length = str && str.length;
-console.log(length);    //''
-
-//옵셔널 체이닝 연산자는 좌항 피연산자가 Falsy값이라도
-//null 또는 undefined가 아니면 우항의 프로퍼티 참조를 이어간다
-length = str?.length;
-console.log(length);    //0
+//null 병합 연산자는 ?? 좌항의 피연산자가 Falsy값이라도
+//null 또는 undefined가 아니면 좌항의 피연산자를 그대로 반환한다.
+foo = '' ?? 'default string';
+console.log(foo);   //''
